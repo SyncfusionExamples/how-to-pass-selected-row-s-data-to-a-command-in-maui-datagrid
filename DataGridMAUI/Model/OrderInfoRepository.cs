65e3c97876cee91298e2fa9ca4f096ca6b06a0c3
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2023-02-28 12:48:02 +0530

    Added KB sample and image

---
+++ DataGridMAUI/Model/OrderInfoRepository.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridMAUI
+{
+    public class OrderInfoRepository
+    {
+        public OrderInfoRepository()
+        {
+        }
+
+        private Random random = new Random();
+
+
+        public ObservableCollection<OrderInfo> GetOrderDetails(int count)
+        {
+            ObservableCollection<OrderInfo> orderDetails = new ObservableCollection<OrderInfo>();
+
+            for (int i = 10001; i <= count + 10000; i++)
+            {
+
+                var ord = new OrderInfo()
+                {
+                    OrderID = i,
+                    EmployeeID = random.Next(1700, 1800).ToString(),
+                    CustomerID = CustomerID[random.Next(15)],
+                    FirstName = FirstNames[random.Next(15)],
+                    LastName = LastNames[random.Next(15)],
+                };
+                orderDetails.Add(ord);
+            }
+            return orderDetails;
+        }
+
+        string[] FirstNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke"
+        };
+
+        string[] LastNames = new string[] {
+            "Adams",
+            "Crowley",
+            "Ellis",
+            "Gable",
+            "Irvine",
+            "Keefe",
+            "Mendoza",
+            "Owens",
+            "Rooney",
+            "Waddell",
+            "Thomas",
+            "Betts",
+            "Doran",
+            "Fitzgerald",
+            "Holmes",
+            "Jefferson",
+            "Landry",
+            "Newberry",
+            "Perez",
+            "Spencer",
+            "Vargas",
+            "Grimes",
+            "Edwards",
+            "Stark",
+            "Cruise",
+            "Fitz",
+            "Chief",
+            "Blanc",
+            "Perry",
+            "Stone",
+            "Williams",
+            "Lane",
+            "Jobs"
+        };
+
+        string[] CustomerID = new string[] {
+            "Alfki",
+            "Frans",
+            "Merep",
+            "Folko",
+            "Simob",
+            "Warth",
+            "Vaffe",
+            "Furib",
+            "Seves",
+            "Linod",
+            "Riscu",
+            "Picco",
+            "Blonp",
+            "Welli",
+            "Folig"
+        };
+    }
+}
